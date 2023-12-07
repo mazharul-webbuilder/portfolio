026b50da69cc1c77004bec035bfdffb4995804ff
IROZEN-DEV3 <mazharul.irozen@gmail.com>
2023-12-07 15:44:15 +0600

    relation made between admin and admin details

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Admin extends Authenticatable
 {
@@ -41,4 +42,12 @@ class Admin extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * Admin Details
+    */
+    public function adminDetail(): HasOne
+    {
+        return $this->hasOne(AdminDetail::class);
+    }
 }
--- app/Models/AdminDetail.php
+++ app/Models/AdminDetail.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class AdminDetail extends Model
 {
     use HasFactory;
+
+    protected $fillable = [];
+
+    public function admin(): BelongsTo
+    {
+        return $this->belongsTo(Admin::class);
+    }
 }
