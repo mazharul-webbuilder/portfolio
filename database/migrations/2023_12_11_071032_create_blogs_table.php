07fbe4e905ed53643c80463ea5b0469a641f7638
IROZEN-DEV3 <mazharul.irozen@gmail.com>
2023-12-11 13:38:11 +0600

    deatabase design

---
+++ database/migrations/2023_12_11_071032_create_blogs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('blog_category_id')->constrained('blog_categories')->cascadeOnDelete();
+            $table->string('title');
+            $table->string('image')->nullable();
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
---
+++ database/migrations/2023_12_11_072206_create_interviews_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('interviews', function (Blueprint $table) {
+            $table->id();
+            $table->string('company_name');
+            $table->string('designation');
+            $table->date('starting_year');
+            $table->date('ending_year');
+            $table->boolean('is_current');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('interviews');
+    }
+};
