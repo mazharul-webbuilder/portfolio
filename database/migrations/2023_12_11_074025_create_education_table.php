8e47352cd91adfcbba6c189cc906fa1a9be6052f
IROZEN-DEV3 <mazharul.irozen@gmail.com>
2023-12-11 13:52:34 +0600

    datbase design complete initial version

---
+++ database/migrations/2023_12_11_074025_create_education_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('education', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('title');
+            $table->date('session_from');
+            $table->date('session_to');
+            $table->double('score');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('education');
+    }
+};
